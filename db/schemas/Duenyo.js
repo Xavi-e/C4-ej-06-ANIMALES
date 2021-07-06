e223fb6ac211763be4cf0d08ec453beba493da7d
Felipe <fgonzalezm75@gmail.com>
2021-07-06 20:58:32 +0200

    schema de duenyo

---
+++ db/schemas/Duenyo.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require("sequelize");
+const Animal = require("./Animal");
+
+const sequelize = require();
+
+const Duenyo = sequelize.define(
+  "Duenyo",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    nombre: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+    },
+    edad: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    dni: {
+      type: DataTypes.STRING(9),
+      unique: true,
+    },
+  },
+  {
+    tableName: "Duenyo",
+    timestamps: false,
+  }
+);
+Animal.hasMany(Duenyo, { foreignKey: "duenyo" });
+Duenyo.belongsTo(Animal, { foreignKey: "duenyo" });
+
+module.exports = Duenyo;
