d8e81666e514f94ddd3e6474f81e7f8a05e6f00c
Felipe <fgonzalezm75@gmail.com>
2021-07-06 20:18:31 +0200

    añadidas carpetas y index.js

---
+++ db/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+
+const usuarioFelipe = process.env.USERNAMEDB;
+const passwordFelipe = process.env.PASSWORDDB;
+
+
+
+const sequelize = new Sequelize({
+    host: "localhost",
+    username: usuarioFelipe,
+    password: passwordFelipe,
+    database: "animales",
+    dialect: "mysql",
+
+});
+// sequelize.authenticate().then(() =>{
+//     console.log("Estamos Dentro!");
+// })
+// .catch((err) => {
+//     console.log("No nos podemos conectar a la base de datos");
+//     console.log(err.message);
+
+// });
