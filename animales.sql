47ffbe8f0a9e9615f280888d96b274ec084b39f1
Profesor X <xesgawork@gmail.com>
2021-07-06 19:03:04 +0200

    commit "incial", problemas con guit, new repo.

---
+++ animales.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF exists Animales;
+CREATE DATABASE Animales;
+use Animales;
+CREATE TABLE Duenyo (
+    id INT AUTO_INCREMENT NOT NULL,
+    nombre VARCHAR(20) NOT NULL,
+    edad INT not null,
+    dni VARCHAR(9) unique not null,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Especie (
+    id INT AUTO_INCREMENT NOT NULL,
+    nombre VARCHAR(20) unique NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Animal (
+    id INT AUTO_INCREMENT NOT NULL,
+    nombre VARCHAR(20) NOT NULL,
+    edad INT not null,
+    dni VARCHAR(9) unique not null,
+    especie int,
+    n_chip int unique not null,
+    duenyo int,
+    PRIMARY KEY (id),
+    foreign key (especie) references Especie(id) on delete set null on update cascade,
+    foreign key (duenyo) references Duenyo(id) on delete set null on update cascade
+);
