b01428033a3fe4053fc062e425d7b9fce13b1a7a
Felipe <fgonzalezm75@gmail.com>
2021-07-06 19:40:52 +0200

    autentificacion mysql

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 require("dotenv").config();
+const { Sequelize } = require("sequelize");
+
+const usuarioFelipe = process.env.USERNAME
+const passwordFelipe = process.env.PASSWORD
+
+
+const sequelize = new Sequelize({
+    host: "localhost",
+    username: usuarioFelipe,
+    password: passwordFelipe,
+    database: "animales",
+    dialect: "mysql",
+
+});
+sequelize.authenticate().then(() =>{
+    console.log("Estamos Dentro!");
+})
+.catch((err) => {
+    console.log("No nos podemos conectar a la base de datos");
+    console.log(err.message);
+
+});
