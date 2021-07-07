849e9084bb0529c0810b5a3cef4bf8c4fc2ae14c
Profesor X <xesgawork@gmail.com>
2021-07-07 13:59:10 +0200

    contenido servidor

---
+++ servidor/rutas/animales.js
@@ -0,0 +1,11 @@
+
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/adoptar/:dni/:idAnimal", async (req, res, next) => {
+    const { dni, idAnimal } = req.params;
+    res.json(`aqui se adopta ${dni} ${idAnimal}`);
+});
+
+module.exports = router;
