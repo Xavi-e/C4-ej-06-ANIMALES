5232badba0dd159a32745fddbe79b7a08b758416
Michaelsp11 <michaelsalgadop11@gmail.com>
2021-07-07 20:56:44 +0200

    Cambiadp especie a perro + preguntar DNI y devuelve id duenyo si existe, sino da error.

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+const chalk = require("chalk");
 const { preguntar } = require("./pregunta/preguntador");
 const {
-  listarAnimalesEspecie,
-} = require("./db/operaciones/operacionesAnimalesPorEspecie");
+  listarMisAnimales,
+} = require("./db/operaciones/operacionListarMisAnimales");
+const { preguntarDNI } = require("./pregunta/preguntas/preguntas");
+const { getDuenyo } = require("./db/operaciones/operacionesDuenyo");
 
-const listar = async () => {
-  const animales = await listarAnimalesEspecie(1);
+const preguntarDNIUsuario = async () => {
+  const respuestas = await preguntar(preguntarDNI);
+  return respuestas;
 };
-listar();
+(async () => {
+  const { dni } = await preguntarDNIUsuario();
+  const duenyo = await getDuenyo(dni);
+  if (!duenyo) {
+    console.log(
+      chalk.red.bold("El DNI introducido no se encuentra registrado!")
+    );
+    process.exit(0);
+  }
+  const idDuenyo = duenyo.toJSON().id;
+})();
